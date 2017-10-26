38d1f745cb1c29add1060a7aab846cc4b6f9a504
GeorSh <shepelevgeorg@gmail.com>
2017-10-26 18:01:11 +0300

    base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "translations#translate"
+
+  resources :users, only: [:new, :create]
+  resources :user_sessions, only: [:create, :destroy]
+
+  get 'logout' => "user_sessions#destroy"
+  
+  get 'login' => 'user_sessions#new'
+  get 'register' => 'users#new'
 end
