82549fcbba53b2a86c6cfef2a0229af3f4cdaa37
GeorSh <shepelevgeorg@gmail.com>
2017-10-28 22:11:52 +0300

    add history

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171026143701) do
+ActiveRecord::Schema.define(version: 20171028190308) do
+
+  create_table "histories", force: :cascade do |t|
+    t.string "word"
+    t.string "translate"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "user_sessions", force: :cascade do |t|
     t.datetime "created_at", null: false
