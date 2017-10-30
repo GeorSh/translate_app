e0a6cf0f5a9849ce8dd5c8a771d018c82f923f88
GeorSh <shepelevgeorg@gmail.com>
2017-10-31 01:12:54 +0300

    add history

--- app/controllers/translations_controller.rb
+++ app/controllers/translations_controller.rb
@@ -2,14 +2,15 @@ class TranslationsController < ApplicationController
 
   helper_method :history
 
-  def translate
+  def translate_index
     client = Client.new 'trnsl.1.1.20170529T154416Z.047bb57fd7a8b3a1.97c5cf0ccade87d10fb983346cfb5703bf811b3e' 
 		lang = params[:priority_lang].present? ? params[:priority_lang] : params[:lang]
     
-    @result = client.submit 'translate?', lang: lang, text: params[:text]
+    result = client.submit 'translate?', lang: lang, text: params[:text]
   	@lang_list = client.submit 'getLangs?', lang: "en"
 		@autodetect = client.submit 'detect?', hint: "en,fr,de", text: params[:text]
-    render :translate
+    @result = result
+    render :translate_index
   end
 
   protected 
